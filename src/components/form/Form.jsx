a4aeaccf363e7576caf691e162457b3a830e2002
aaksant <aaksangit@gmail.com>
2025-01-05 16:39:25 +0700

    feat: implement dropdown feature

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,20 +1,39 @@
+import { useState } from 'react';
+import { SlArrowDown } from 'react-icons/sl';
 import '../../styles/Form.css';
 
-export default function Form({ title, fields, formId }) {
+export default function Form({ title, fields }) {
+  const [isFormShown, setIsFormShown] = useState(false);
+
+  function handleFormDropdown() {
+    setIsFormShown(!isFormShown);
+  }
+
   return (
-    <div className="form">
+    <div
+      className={`form ${isFormShown ? 'active' : ''}`}
+      onClick={handleFormDropdown}
+    >
       <div className="form__header">
         <h2>{title}</h2>
-        <button type="button">down</button>
+        <button
+          type="button"
+          className="form__dropdown-button"
+          onClick={handleFormDropdown}
+        >
+          <SlArrowDown className="icon" />
+        </button>
       </div>
-      <form action="/" id={`form-${formId}`}>
-        {fields.map(({ label, type, name, id }, index) => (
-          <div className="form-row" key={index}>
-            <label htmlFor={name}>{label}</label>
-            <input type={type} name={name} id={id} />
-          </div>
-        ))}
-      </form>
+      {isFormShown && (
+        <form action="/">
+          {fields.map(({ label, type, name, id }, index) => (
+            <div className="form-row" key={index}>
+              <label htmlFor={name}>{label}</label>
+              <input type={type} name={name} id={id} />
+            </div>
+          ))}
+        </form>
+      )}
     </div>
   );
 }
