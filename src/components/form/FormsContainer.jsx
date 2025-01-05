7f56ee551fae586f2bdefecc5c5a9c74a830a093
aaksant <aaksangit@gmail.com>
2025-01-05 16:38:59 +0700

    refactor: remove index for form id

--- src/components/form/FormsContainer.jsx
+++ src/components/form/FormsContainer.jsx
@@ -5,8 +5,8 @@ import '../../styles/FormsContainer.css';
 export default function FormsContainer() {
   return (
     <section className="forms-container">
-      {formsConfig.map(({ title, id, fields }, index) => (
-        <Form title={title} fields={fields} key={id} formId={index + 1} />
+      {formsConfig.map(({ title, id, fields }) => (
+        <Form title={title} fields={fields} key={id} />
       ))}
     </section>
   );
