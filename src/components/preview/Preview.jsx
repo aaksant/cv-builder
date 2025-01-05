035a61079f8ddc49446abf8b47620ef318c70c33
aaksant <aaksangit@gmail.com>
2025-01-05 15:40:04 +0700

    feat: add initial preview component

---
+++ src/components/preview/Preview.jsx
@@ -0,0 +1,12 @@
+import '../../styles/Preview.css';
+
+export default function Preview() {
+  return (
+    <section className="preview-container">
+      <header>
+        <h1></h1>
+      </header>
+      <main></main>
+    </section>
+  );
+}
