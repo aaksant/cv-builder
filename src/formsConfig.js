e9c6d0d006652851092209c29c77f975f9cabac7
aaksant <aaksangit@gmail.com>
2025-01-06 19:22:46 +0700

    feat: add `isRequired` property for required inputs

--- src/formsConfig.js
+++ src/formsConfig.js
@@ -3,15 +3,28 @@ const formsConfig = [
     id: crypto.randomUUID(),
     title: 'Personal Details',
     fields: [
-      { label: 'Full Name', type: 'text', name: 'fullName', id: 'fullName' },
+      {
+        label: 'Full Name',
+        type: 'text',
+        name: 'fullName',
+        id: 'fullName',
+        isRequired: true
+      },
       {
         label: 'Phone Number',
         type: 'tel',
         name: 'phoneNumber',
-        id: 'phoneNumber'
+        id: 'phoneNumber',
+        isRequired: true
       },
       { label: 'Email', type: 'text', name: 'email', id: 'email' },
-      { label: 'Address', type: 'text', name: 'address', id: 'address' }
+      {
+        label: 'Address',
+        type: 'text',
+        name: 'address',
+        id: 'address',
+        isRequired: true
+      }
     ]
   }
 ];
