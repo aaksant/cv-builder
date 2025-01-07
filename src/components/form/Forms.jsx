ddd25f2abd429494b30710c06f663e2834384d9a
aaksant <aaksangit@gmail.com>
2025-01-07 19:14:20 +0700

    refactor: separate forms with single entry and forms with multiple entries

--- src/components/form/Forms.jsx
+++ src/components/form/Forms.jsx
@@ -1,12 +1,19 @@
-import Form from './Form';
+import SingleEntryForm from './SingleEntryForm';
+import MultipleEntriesForm from './MultipleEntriesForm';
 import formsConfig from '../../formsConfig';
 import '../../styles/Forms.css';
 
 export default function Forms() {
+  const objectStateForm = formsConfig.filter((form) => form.state === 'object');
+  const arrayStateForm = formsConfig.filter((form) => form.state !== 'object');
+
   return (
     <section className="forms">
-      {formsConfig.map(({ title, id, fields }) => (
-        <Form title={title} fields={fields} key={id} />
+      {objectStateForm.map(({ title, id, fields }) => (
+        <SingleEntryForm title={title} fields={fields} key={id} />
+      ))}
+      {arrayStateForm.map(({ title, id, fields }) => (
+        <MultipleEntriesForm id={id} title={title} fields={fields} key={id} />
       ))}
     </section>
   );
