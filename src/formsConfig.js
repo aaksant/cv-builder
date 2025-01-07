c858710004e7b4a0befbfa778ed6b09c522f1950
aaksant <aaksangit@gmail.com>
2025-01-07 19:10:13 +0700

    feat: add second form config

--- src/formsConfig.js
+++ src/formsConfig.js
@@ -1,6 +1,7 @@
 const formsConfig = [
   {
     id: crypto.randomUUID(),
+    state: 'object',
     title: 'Personal Details',
     fields: [
       {
@@ -24,6 +25,48 @@ const formsConfig = [
         name: 'address',
         id: 'address',
         isRequired: true
+      }
+    ]
+  },
+  {
+    id: crypto.randomUUID(),
+    state: 'array',
+    title: 'Educations',
+    fields: [
+      {
+        label: 'University',
+        type: 'text',
+        name: 'university',
+        id: 'university',
+        isRequired: false
+      },
+      {
+        label: 'Start Year',
+        type: 'text',
+        name: 'startYear',
+        id: 'startYear',
+        isRequired: false
+      },
+      {
+        label: 'Graduate Year',
+        type: 'text',
+        name: 'gradYear',
+        id: 'gradeYear',
+        isRequired: false
+      },
+      {
+        label: 'Degree',
+        type: 'text',
+        name: 'degree',
+        id: 'degree',
+        isRequired: false
+      },
+      {
+        label: 'Grade',
+        type: 'text',
+        name: 'grade',
+        id: 'grade',
+        isRequired: false
       }
     ]
   }
