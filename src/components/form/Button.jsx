d8b74af5b9d17e7a282536c0a9c1a9b9c462bc30
aaksant <aaksangit@gmail.com>
2025-01-07 21:41:26 +0700

    refactor: change component name to `FormControlButton`

--- src/components/form/Button.jsx
+++ src/components/form/Button.jsx
@@ -1,4 +1,4 @@
-export default function Button({ className, text, onClick, type = 'button' }) {
+export default function FormControlButton({ className, text, onClick, type = 'button' }) {
   return (
     <button className={`btn ${className}`} onClick={onClick} type={type}>
       {text}
