9c4c9e99772933ff50cd8d88cc3115aa785ec1f8
aaksant <aaksangit@gmail.com>
2025-01-15 23:11:44 +0700

    refactor: add form entries to props for clearer conditional rendering

--- src/components/form/NewEntryPrompt.jsx
+++ src/components/form/NewEntryPrompt.jsx
@@ -1,12 +1,14 @@
-export default function NoEntriesPrompt({ handleNewEntry }) {
+export default function NewEntryPrompt({ entries, handleNewEntry }) {
   return (
     <div className="message">
-      <p
-        className="p-light"
-        style={{ textAlign: 'center', marginBottom: '1rem' }}
-      >
-        No current entries
-      </p>
+      {entries.length === 0 && (
+        <p
+          className="p-light"
+          style={{ textAlign: 'center', marginBottom: '1rem' }}
+        >
+          No current entries
+        </p>
+      )}
       <button
         type="button"
         className="btn btn-new-entry"
