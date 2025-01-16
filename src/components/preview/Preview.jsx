89e70c7d9fbbbfd1633bc2d6f9a23a0ce64d48b0
aaksant <aaksangit@gmail.com>
2025-01-17 00:15:22 +0700

    feat: add `MainContent` component

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,13 +1,14 @@
 import '../../styles/Preview.css';
 import Header from './Header';
+import MainContent from './MainContent';
 
 export default function Preview({ cvData }) {
-  const { personalDetails } = cvData;
+  const { personalDetails, educations } = cvData;
 
   return (
     <section className="preview-container">
       <Header personalDetails={personalDetails} />
-      <main></main>
+      <MainContent educations={educations} />
     </section>
   );
 }
