4c10501006dae7887aa103ea82ad98ccdd4a8039
aaksant <aaksangit@gmail.com>
2025-01-17 16:10:41 +0700

    feat: add `textarea` tag

--- src/components/form/FormRow.jsx
+++ src/components/form/FormRow.jsx
@@ -1,8 +1,19 @@
-export default function FormRow({ label, name, type, id, isRequired }) {
+export default function FormRow({
+  isInput,
+  label,
+  name,
+  type,
+  id,
+  isRequired
+}) {
   return (
     <div className="form-row">
       <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} id={id} required={isRequired} />
+      {isInput ? (
+        <input type={type} name={name} id={id} required={isRequired} />
+      ) : (
+        <textarea name={name} id={id}></textarea>
+      )}
     </div>
   );
 }
