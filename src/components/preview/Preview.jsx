29215b80afac17afe1fe34e313613691521989f3
aaksant <aaksangit@gmail.com>
2025-01-17 22:21:45 +0700

    feat: add experiences section as prop for `MainContent`

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -3,12 +3,12 @@ import Header from './Header';
 import MainContent from './MainContent';
 
 export default function Preview({ cvData }) {
-  const { personalDetails, educations } = cvData;
+  const { personalDetails, educations, experiences } = cvData;
 
   return (
     <section className="preview-container">
       <Header personalDetails={personalDetails} />
-      <MainContent educations={educations} />
+      <MainContent educations={educations} experiences={experiences} />
     </section>
   );
 }
