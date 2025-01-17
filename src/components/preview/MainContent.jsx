04693a9e0d03c49ee16fef70a7293ca23e4c25dd
aaksant <aaksangit@gmail.com>
2025-01-17 22:22:31 +0700

    feat: add conditional rendering to make sure that first section filled is placed right after `Header`

--- src/components/preview/MainContent.jsx
+++ src/components/preview/MainContent.jsx
@@ -1,9 +1,19 @@
 import Educations from './Educations';
+import Experiences from './Experiences';
 
-export default function MainContent({ educations }) {
+export default function MainContent({ educations, experiences }) {
+  const hasEducations = educations.length > 0;
+  const hasExperiences = experiences.length > 0;
+
   return (
     <main>
-      <Educations educations={educations} />
+      {hasEducations && <Educations educations={educations} />}
+      {hasExperiences && <Experiences experiences={experiences} />}
+      {!hasEducations && !hasExperiences && (
+        <p className="text-light" style={{ textAlign: 'center' }}>
+          No entries added yet.
+        </p>
+      )}
     </main>
   );
 }
