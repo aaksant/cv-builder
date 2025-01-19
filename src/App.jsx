02fc9888e109bf4f4e0634d990ecbe5a4df4c7cb
aaksant <aaksangit@gmail.com>
2025-01-19 20:10:06 +0700

    feat: implement `localStorage` to save data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Forms from './components/form/Forms';
 import Preview from './components/preview/Preview';
 import './styles/App.css';
 
 export default function App() {
-  const [cvData, setCvData] = useState({
-    personalDetails: {},
-    educations: [],
-    experiences: []
-  });
+  const [cvData, setCvData] = useState(
+    JSON.parse(localStorage.getItem('cvData')) ?? {
+      personalDetails: {},
+      educations: [],
+      experiences: []
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem('cvData', JSON.stringify(cvData));
+  }, [cvData]);
 
   return (
     <div className="wrapper">
